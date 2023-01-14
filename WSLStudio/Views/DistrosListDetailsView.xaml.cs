dfa87382acfd6cd08f54695a69dacff9231c29e7
NathanQuellec <71017057+NathanQuellec@users.noreply.github.com>
2023-01-14 19:45:16 +0100

    change infobar :Name

--- WSLStudio/Views/DistrosListDetailsView.xaml.cs
+++ WSLStudio/Views/DistrosListDetailsView.xaml.cs
@@ -49,7 +49,7 @@ public sealed partial class DistrosListDetailsView : Page
         //Close InfoBar after timer set in DistroListDetailsViewModel.cs:RemoveSuccessInfoBar()
         WeakReferenceMessenger.Default.Register<CloseInfoBarMessage>(this, (recipient, message) =>
         {
-            var dispatcher = DispatcherQueue.TryEnqueue(() => RemoveDistroSuccess.IsOpen = false);
+            var dispatcher = DispatcherQueue.TryEnqueue(() => RemoveDistroInfoSuccess.IsOpen = false);
         });
     }
 
